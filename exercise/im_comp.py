70b041d39ad5019133f98b8fcbbd222f94347eac
Rui Wang <rwa56@lib7000-rcmac03.lib.sfu.ca>
2017-02-15 23:41:25 -0800

    test

--- exercise/im_comp.py
+++ exercise/im_comp.py
@@ -6,19 +6,19 @@ import cPickle as pickle
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.models import Model
-from keras.layers import Input, Dense, Embedding, Reshape, GRU, merge, LSTM, Dropout, BatchNormalization, Activation
+from keras.layers import Input, Dense, Embedding, Reshape, GRU, merge, LSTM, Dropout, BatchNormalization, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
+from keras.optimizers import RMSprop, Adagrad
+from keras.regularizers import l2, activity_l2
 
 
-batch_size = 12
+batch_size = 32
 nb_classes = 10
-nb_epoch = 200
+nb_epoch = 50
 data_augmentation = True
 img_rows, img_cols = 32, 32
 img_channels = 3
-
-
 
 
 feat_train = open('/Users/rwa56//Downloads/Homework2_data/train_feat.pickle', 'rb')
@@ -29,7 +29,6 @@ label_train = open('/Users/rwa56//Downloads/Homework2_data/train_lab.pickle', 'r
 train_label = pickle.load(label_train)
 label_train.close()
 
-
 feat_valid = open('/Users/rwa56//Downloads/Homework2_data/validation_feat.pickle', 'rb')
 valid_feat = pickle.load(feat_valid)
 feat_valid.close()
@@ -38,14 +37,31 @@ label_valid = open('/Users/rwa56//Downloads/Homework2_data/validation_lab.pickle
 valid_label = pickle.load(label_valid)
 label_valid.close()
 
-
-feat_test = open('/Users/rwa56//Downloads/Homework2_data/train_feat.pickle', 'rb')
+feat_test = open('/Users/rwa56//Downloads/Homework2_data/test_feat.pickle', 'rb')
 test_feat = pickle.load(feat_test)
 feat_test.close()
 
 
 print('The train shape: ', train_feat.shape)
 print(train_feat.shape[0], 'train samples')
+
+print('The validation shape: ', valid_feat.shape)
+print(valid_feat.shape[0],'test samples')
+
+
+print('The test shape: ', test_feat.shape)
+print(test_feat.shape[0],'test samples')
+
+X_train = train_feat.astype('float32')
+X_valid = valid_feat.astype('float32')
+X_test = test_feat.astype('float32')
+
+X_train /= 255
+X_valid /= 255
+X_test /= 255
+
+
+print('Train Sample Shape: ', X_train.shape[1:])
 
 Y_train = np_utils.to_categorical(train_label, nb_classes)
 Y_valid = np_utils.to_categorical(valid_label, nb_classes)
@@ -54,40 +70,57 @@ Y_valid = np_utils.to_categorical(valid_label, nb_classes)
 
 model = Sequential()
 
-model.add(Convolution2D(32, 3, 3, border_mode='same',
-                                                input_shape=train_feat.shape[1:]))
-model.add(Activation('relu'))
-model.add(Convolution2D(32, 3, 3))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Convolution2D(64, 3, 3, border_mode='same'))
+model.add(Convolution2D(64, 3, 3, border_mode='same',
+                input_shape=X_train.shape[1:]))
+model.add(BatchNormalization())
 model.add(Activation('relu'))
 model.add(Convolution2D(64, 3, 3))
+model.add(BatchNormalization())
 model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
 model.add(Dropout(0.25))
 
-model.add(Flatten())
-model.add(Dense(512))
+model.add(Convolution2D(128, 3, 3, border_mode='same'))
+model.add(BatchNormalization())
 model.add(Activation('relu'))
-model.add(Dropout(0.5))
+model.add(Convolution2D(256, 3, 3))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2),strides=(2, 2)))
+model.add(Dropout(0.25))
+
+
+model.add(Convolution2D(256, 3, 3, border_mode='same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Convolution2D(512, 3, 3))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2),strides=(2, 2)))
+model.add(Dropout(0.25))
+
+
+
+model.add(Flatten())
+model.add(Dense(512,input_dim=512, W_regularizer=l2(0.01), activity_regularizer=activity_l2(0.01)))
+model.add(Activation('relu'))
+model.add(Dropout(0.30))
 model.add(Dense(nb_classes))
 model.add(Activation('softmax'))
 
+adagrad = Adagrad(lr=0.01, epsilon=1e-08, decay=0.0)
 model.compile(loss='categorical_crossentropy',
-              optimizer='rmsprop',
+              optimizer=adagrad,
               metrics=['accuracy'])
 
-X_train = train_feat.astype('float32')
-X_valid = train_valid.astype('float32')
-X_train /= 255
-X_valid /= 255
 
 
 if not data_augmentation:
     print('Not using data augmentation.')
+    model.fit(X_train, Y_train, batch_size=batch_size,
+                        nb_epoch=nb_epoch,
+                        verbose=1,
+                        validation_data=(X_valid,Y_valid))
 
 else:
     print('Use real-time data augmentation')
@@ -95,9 +128,9 @@ else:
     datagen = ImageDataGenerator(
         featurewise_center=False,
         samplewise_center=False,
-        feature_std_normalization=False,
+        featurewise_std_normalization=False,
         samplewise_std_normalization=False,
-        zca_whitening=False,
+        zca_whitening=True,
         rotation_range=0,
         width_shift_range=0.1,
         height_shift_range=0.1,
@@ -106,15 +139,15 @@ else:
 
     datagen.fit(X_train)
 
+    model.fit_generator(datagen.flow(X_train, Y_train,
+                                     batch_size=batch_size),
+                        samples_per_epoch=X_train.shape[0],
+                        nb_epoch=nb_epoch,
+                        validation_data=(X_valid,Y_valid))
 
 
-
+test_label = model.predict(X_test)
 
 label_test = open('~/Downloads/Homework2_data/test_lab.pickle', 'rb')
 pickle.dump(test_label, label_test)
 label_test.close()
-
-
-
-
-
